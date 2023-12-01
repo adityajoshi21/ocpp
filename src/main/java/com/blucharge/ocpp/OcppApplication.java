c72aa28557130190edb45d838e9554386f58401c
rajatsingh <rajat.singh@blu-smart.com>
2023-12-01 08:48:45 +0530

    fixing issue

--- src/main/java/com/blucharge/ocpp/OcppApplication.java
+++ src/main/java/com/blucharge/ocpp/OcppApplication.java
@@ -2,8 +2,10 @@ package com.blucharge.ocpp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.blucharge.core", "com.blucharge.ocpp", "com.blucharge.auth", "com.blucharge.event", "com.blucharge.util", "com.blucharge.exception"})
 public class OcppApplication {
 
     public static void main(String[] args) {
